d5c143ce9bf84723136ca94f3aaacc47ca202854
niyuhnuh <nianiaandrea@gmail.com>
2021-05-23 13:56:35 +0800

    Trying operators

--- OOP-Calculator/Form1.cs
+++ OOP-Calculator/Form1.cs
@@ -12,7 +12,8 @@ namespace OOP_Calculator
 {
     public partial class Form1 : Form
     {
-        Double value = 0;  
+        Double input1 = 0;
+        String operatorUse = ""; 
         
         public Form1()
         {
@@ -24,7 +25,7 @@ namespace OOP_Calculator
         {
             txtbxOutput.Clear();
             txtbxOutput.Text = "0";
-            value = 0; 
+            input1 = 0; 
         }
 
         private void back_Btn_Click(object sender, EventArgs e)
@@ -52,22 +53,30 @@ namespace OOP_Calculator
 
         private void add_Sign_Click(object sender, EventArgs e)
         {
-
+            operatorUse = add_Sign.Text;
+            input1 = Double.Parse(txtbxOutput.Text);
+            txtbxOutput.Clear();
         }
 
         private void sub_Sign_Click(object sender, EventArgs e)
         {
-
+            operatorUse = sub_Sign.Text;
+            input1 = Double.Parse(txtbxOutput.Text);
+            txtbxOutput.Clear();
         }
 
         private void mul_Sign_Click(object sender, EventArgs e)
         {
-
+            operatorUse = mul_Sign.Text;
+            input1 = Double.Parse(txtbxOutput.Text);
+            txtbxOutput.Clear();
         }
 
         private void div_Sign_Click(object sender, EventArgs e)
         {
-
+            operatorUse = div_Sign.Text;
+            input1 = Double.Parse(txtbxOutput.Text);
+            txtbxOutput.Clear();
         }
     }
 }
