9520c814be462bac94b06a4dfa288ac74c37c7b1
niyuhnuh <nianiaandrea@gmail.com>
2021-06-23 11:27:41 +0800

    Memory plus button function (7)

--- OOP-Calculator/Form1.cs
+++ OOP-Calculator/Form1.cs
@@ -169,15 +169,6 @@ namespace OOP_Calculator
                             txtbxOutput.Text = "0";
                             break;
                         }
-                        //else if (memoryPlusIsClicked == true)
-                        {
-                            //double input3 = Double.Parse(txtbx_Saved1.Text);
-                            //answer = input1 + input3;
-                            //lbl_View.Text = answer.ToString();
-                            //input1 = answer;
-                            //txtbxOutput.Text = "0";
-                            //break;
-                        }
                         else
                         {
                             double percent;
