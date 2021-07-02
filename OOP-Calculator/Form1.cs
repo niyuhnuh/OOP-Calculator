fc8b436a91c62acbd2a77b1caa209cbf3cc99a8b
niyuhnuh <nianiaandrea@gmail.com>
2021-07-02 21:27:59 +0800

    Multiplication class operations

--- OOP-Calculator/Form1.cs
+++ OOP-Calculator/Form1.cs
@@ -139,8 +139,9 @@ namespace OOP_Calculator
         {
             if (input1 != 0)
             {
-                equals_Btn.PerformClick(); 
-                operatorUse = mul_Sign.Text; 
+                double answer;
+                answer = input1 * Double.Parse(txtbxOutput.Text);
+                input1 = answer;
                 txtbxOutput.Clear();
                 lbl_View.Text = lbl_View.Text + mul_Sign.Text;
             }
