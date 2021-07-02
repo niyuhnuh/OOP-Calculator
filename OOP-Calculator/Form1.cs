432890103f27b685478efa448bf83301254ee230
niyuhnuh <nianiaandrea@gmail.com>
2021-07-02 08:11:19 +0800

    Multiplication view label history revision

--- OOP-Calculator/Form1.cs
+++ OOP-Calculator/Form1.cs
@@ -245,26 +245,21 @@ namespace OOP_Calculator
                         }
                        
                     case "*"://Multiplication
-                        //This statement checks if the percent button is clicked
                         if (percentIsClicked == false)
                         {
                             answer = (input1 * input2);
-                            lbl_View.Text = answer.ToString();
                             input1 = answer;
-                            txtbxOutput.Text = "0";
+                            txtbxOutput.Text = answer.ToString();
                             break;
                         }
-                        //When it is, it converts the second input to a percent similar to the function of 
-                        //a regular calculator
                         else
                         {
                             double percent;
                             percent = (input2 / 100);
 
-                            answer = input1 * percent;
-                            lbl_View.Text = answer.ToString();
+                            answer = input1 * percent; 
                             input1 = answer;
-                            txtbxOutput.Text = "0";
+                            txtbxOutput.Text = answer.ToString();
                             break;
                         }
                             
